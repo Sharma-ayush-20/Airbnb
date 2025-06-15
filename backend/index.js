92205750e784bb72005c9634a0ce4d4b6d5644b8
Sharma-ayush-20 <149467415+Sharma-ayush-20@users.noreply.github.com>
2025-06-15 23:44:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://airbnb-frontend-pknj.onrender.com",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 }
